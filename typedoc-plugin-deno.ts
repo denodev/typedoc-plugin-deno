e18e3cd8b8b87855a0be157fbbdde2d50d88e22b
迷渡 <justjavac@gmail.com>
2020-04-21 12:59:26 +0800

    suppot param i18n
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- typedoc-plugin-deno.ts
+++ typedoc-plugin-deno.ts
@@ -1,118 +1,145 @@
-import * as ts from 'typescript';
 import { trim } from 'lodash';
 import { Component, ConverterComponent } from 'typedoc/dist/lib/converter/components';
 import { Context } from 'typedoc/dist/lib/converter/context';
 import { Converter } from 'typedoc/dist/lib/converter/converter';
-import { Comment, CommentTag } from 'typedoc/dist/lib/models';
-import { Reflection } from 'typedoc/dist/lib/models/reflections/abstract';
-import {
-  ParameterReflection,
-  TypeParameterReflection,
-  SignatureReflection,
-  DeclarationReflection,
-} from 'typedoc/dist/lib/serialization/schema';
+import { Comment, CommentTag, SignatureReflection } from 'typedoc/dist/lib/models';
+import { Reflection, ReflectionKind } from 'typedoc/dist/lib/models/reflections/abstract';
 
 @Component({ name: 'deno' })
 export class DenoPlugin extends ConverterComponent {
   initialize() {
     this.listenTo(this.owner, {
-      [Converter.EVENT_CREATE_DECLARATION]: this.onDeclaration,
-      [Converter.EVENT_CREATE_SIGNATURE]: this.onSignature,
-      [Converter.EVENT_CREATE_PARAMETER]: this.onParameter,
-      [Converter.EVENT_CREATE_TYPE_PARAMETER]: this.TypeParameter,
+      [Converter.EVENT_RESOLVE_END]: this.onEndResolve,
     });
   }
 
-  /**
-   * Triggered when the converter has created a parameter reflection.
-   * @todo Maybe not usefull
-   *
-   * @param context  The context object describing the current state the converter is in.
-   * @param reflection  The reflection that is currently processed.
-   * @param node  The node that is currently processed if available.
-   */
-  private onParameter(context: Context, reflection: ParameterReflection, node?) {
-    if (!reflection.comment) return;
-    const comment = reflection.comment;
-  }
-
-  /**
-   * Triggered when the converter has created a type parameter reflection.
-   * @todo Maybe not usefull
-   *
-   * @param context  The context object describing the current state the converter is in.
-   * @param reflection  The reflection that is currently processed.
-   * @param node  The node that is currently processed if available.
-   */
-  private TypeParameter(context: Context, reflection: Reflection, node?) {
-    if (!reflection.comment) return;
-    const comment = reflection.comment;
-  }
-
-  /**
-   * Triggered when the converter has created a declaration reflection.
-   *
-   * @param context  The context object describing the current state the converter is in.
-   * @param reflection  The reflection that is currently processed.
-   * @param node  The node that is currently processed if available.
-   */
-  private onSignature(context: Context, reflection: Reflection, node?) {
-    if (!reflection.comment) return;
-    this.onDeclaration(context, reflection, node);
-
-    if (!reflection.comment.tags) return;
-    const tags = reflection.comment.tags as [CommentTag & I18nTag];
-    let i = 0;
-    let c = tags.length;
-    while (i < c - 1) {
-      if (tags[i].tagName + '_i18n' === tags[i + 1].tagName) {
-        tags[i].text_i18n = tags[i + 1].text.replace(tags[i].paramName, '').trim();
-        tags.splice(i + 1, 1);
-        c--;
+  /** Triggered when the converter has finished resolving a project. */
+  private onEndResolve(context: Context) {
+    for (let signature of Object.values(context.project.reflections)) {
+      if (!isSignature(signature)) {
+        continue;
       }
-      i++;
-    }
-  }
 
-  /**
-   * Triggered when the converter has created a declaration reflection.
-   *
-   * @param context  The context object describing the current state the converter is in.
-   * @param reflection  The reflection that is currently processed.
-   * @param node  The node that is currently processed if available.
-   */
-  private onDeclaration(context: Context, reflection: Reflection, node?) {
-    if (!reflection.comment) return;
+      const comment = signature.comment as Comment & I18nComment;
+      if (!comment) {
+        continue;
+      }
 
-    const comment = reflection.comment as Comment & I18nComment;
-    comment.text = trim(comment.text, '\n');
+      processi18nLineByLine(comment);
 
-    if (!comment.tags) return;
+      if (comment && comment.hasTag('returns_i18n')) {
+        comment.returns_i18n = comment.getTag('returns_i18n')!.text;
+        comment.removeTags('returns_i18n');
+      }
 
-    const i18nTag = comment.tags.find((x) => (x.tagName = '_i18n'));
+      if (!signature.parameters) {
+        continue;
+      }
 
-    if (i18nTag === undefined) return;
+      for (let parameter of signature.parameters) {
+        const tag = getI18nParamTag(comment, parameter.name);
+        if (!tag) {
+          continue;
+        }
+        (parameter.comment as Comment & I18nComment).text_i18n = tag.text.replace(parameter.name, '').trim();
+        (parameter.comment as Comment & I18nComment).shortText_i18n = '';
+      }
+      comment.removeTags('param_i18n');
 
-    const i18n = trim(i18nTag.text, '\n');
-    const firstLF = i18n.indexOf('\n\n');
+      if (!signature.typeParameters) {
+        continue;
+      }
 
-    if (firstLF === -1) {
-      comment.shortText_i18n = i18n;
-      comment.text_i18n = '';
-    } else {
-      comment.shortText_i18n = i18n.substr(0, firstLF);
-      comment.text_i18n = i18n.substr(firstLF + 2);
-    }
-
-    removeTags(comment, '_i18n');
-    if (isEmptyComment(comment)) {
-      delete reflection.comment;
+      for (let parameter of signature.typeParameters) {
+        const tag = getI18nTypeParamTag(comment, parameter.name);
+        if (!tag) {
+          continue;
+        }
+        (parameter.comment as Comment & I18nComment).text_i18n = tag.text.replace(parameter.name, '').trim();
+        (parameter.comment as Comment & I18nComment).shortText_i18n = '';
+      }
+      comment.removeTags('typeparam_i18n');
+      comment.removeTags('template_i18n');
     }
   }
 }
 
-function isEmptyComment(comment: Comment) {
-  return !comment || (!comment.text && !comment.shortText && (!comment.tags || comment.tags.length === 0));
+function processi18nLineByLine(comment: Comment & I18nComment): void {
+  if (!comment) {
+    return;
+  }
+
+  comment.text = trim(comment.text, '\n');
+
+  if (!comment.tags) {
+    return;
+  }
+
+  const tags = comment.tags;
+
+  comment.text_line_i18n = [];
+  comment.text_line_en = [];
+
+  comment.shortText_i18n = '';
+
+  let i = 0;
+  let c = tags.length;
+  while (i < c) {
+    const text = trim(tags[i].text, '\n');
+    if (tags[i].tagName === 'i18n' && text !== '') {
+      const [one, two, ...others] = text.split('\n\n');
+
+      comment.text_line_i18n.push(one);
+      comment.text_line_en.push(two || '');
+
+      for (let line of others) {
+        comment.text_line_en.push(line);
+        comment.text_line_i18n.push('');
+      }
+    }
+
+    if (tags[i].tagName === 'i18n') {
+      // Remove tag
+      tags.splice(i, 1);
+      c--;
+    } else {
+      // Process next tag
+      i++;
+    }
+  }
+
+  if (comment.text_line_i18n.length === 0) {
+    return;
+  }
+
+  if (comment.text === '') {
+    comment.shortText_i18n = comment.text_line_i18n.shift();
+    comment.text = comment.text_line_en.join('\n\n');
+  } else {
+    comment.text_line_i18n.unshift(comment.text);
+    comment.text = comment.text_line_en.join('\n\n');
+  }
+  comment.text_line_en.pop();
+}
+
+function isSignature(reflection: Reflection): reflection is SignatureReflection {
+  return reflection.kindOf(ReflectionKind.SomeSignature);
+}
+
+function getI18nParamTag(comment: Comment, paramName?: string): CommentTag | undefined {
+  if (!comment.tags) {
+    return undefined;
+  }
+  return comment.tags.find((tag) => tag.tagName === 'param_i18n' && tag.text.startsWith(paramName));
+}
+
+function getI18nTypeParamTag(comment: Comment, paramName?: string): CommentTag | undefined {
+  if (!comment.tags) {
+    return undefined;
+  }
+  return comment.tags.find(
+    (tag) => (tag.tagName === 'typeparam_i18n' || tag.tagName === 'template_i18n') && tag.text.startsWith(paramName),
+  );
 }
 
 interface I18nTag {
@@ -121,27 +148,7 @@ interface I18nTag {
 
 interface I18nComment extends I18nTag {
   shortText_i18n: string;
-}
-
-/**
- * Remove all tags with the given name from the given comment instance.
- *
- * @param comment  The comment that should be modified.
- * @param tagName  The name of the that that should be removed.
- */
-function removeTags(comment: Comment | undefined, tagName: string) {
-  if (!comment || !comment.tags) {
-    return;
-  }
-
-  let i = 0,
-    c = comment.tags.length;
-  while (i < c) {
-    if (comment.tags[i].tagName === tagName) {
-      comment.tags.splice(i, 1);
-      c--;
-    } else {
-      i++;
-    }
-  }
+  text_line_i18n: string[];
+  text_line_en: string[];
+  returns_i18n: string;
 }
